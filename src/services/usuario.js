29304fe0e04074216f7ced105ef2454ea2c55937
Juan Carlos <juancarlos.gonzalezhuerta@aol.com>
2022-08-08 18:38:17 -0500

    feat: functions for Users were added

--- src/services/usuario.js
+++ src/services/usuario.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//Registrar Usuario
 export const registrarUsuario = async (usuario) => {
     const respuesta = await axios.post('/usuario',{
         email: usuario,
@@ -7,6 +8,7 @@ export const registrarUsuario = async (usuario) => {
     return respuesta;
 }
 
+//Buscar Usuario
 export const buscarUsuario = async (usuario) => {
     const respuesta = await axios.get(`/usuario/${usuario}`);
     return respuesta;
